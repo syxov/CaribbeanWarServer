2751b428c36fe7dcbe816fad23c58d0c84891f4f
Aliaksandr Zasim <al.zasim@gmail.com>
2014-10-04 18:18:17 +0300

    Try to use heroku

---
+++ main.go
@@ -0,0 +1,33 @@
+// CaribbeanWar project main.go
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if conn, err := upgrader.Upgrade(w, r, nil); err == nil {
+		for {
+			messageType, p, _ := conn.ReadMessage()
+			conn.WriteMessage(messageType, p)
+		}
+	} else {
+		log.Print(err)
+	}
+}
+
+func main() {
+	log.Print("Server started")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":9000", nil)
+}
