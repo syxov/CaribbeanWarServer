1aa8e6bd64cc11370e2958c47110c111391e4799
Aliaksandr Zasim <al.zasim@gmail.com>
2014-10-07 10:02:21 +0300

    do not send responce to email

--- api/socket.go
+++ api/socket.go
@@ -5,7 +5,6 @@ import (
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
-	"net/smtp"
 	"time"
 )
 
@@ -22,10 +21,7 @@ var upgrader = websocket.Upgrader{
 func Handler(w http.ResponseWriter, r *http.Request) {
 	defer func() {
 		if err := recover(); err != nil {
-			convertedErr := *(err.(*error))
-			auth := smtp.PlainAuth("", "al.syxov@gmail.com", "505604qw", "smtp.gmail.com")
-			smtp.SendMail("smtp.gmail.com:587", auth, "server", []string{"al.syxov@gmail.com"}, []byte(convertedErr.Error()))
-			log.Print(convertedErr.Error())
+			log.Print(err)
 		}
 	}()
 	conn, _ := upgrader.Upgrade(w, r, nil)
